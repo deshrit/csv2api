571bcc28a87fff4f68c36ba7b64d6d877932ac91
deshrit baral <deshritbaral@gmail.com>
2023-07-20 11:45:38 +0545

    Added custom user model - unique email address (#2)

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "is_staff", "is_active"]
+
+admin.site.register(CustomUser, CustomUserAdmin)
